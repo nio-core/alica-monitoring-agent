196c29cdd0f5aa587b339da2c057587d2995c1af
Sven Starcke <sst97@gmx.net>
2020-08-06 20:00:36 +0200

    adds conversion model for SolverVar

--- test/include/test_values_common.h
+++ test/include/test_values_common.h
@@ -14,11 +14,11 @@ constexpr const int AGENT_COUNT = 3;
 
 // Allocation Authority Info
 constexpr const int PARENT_STATE = 1;
-constexpr const int  PLAN_TYPE = 1;
-constexpr const int  PLAN_ID = 1;
-constexpr const int  ENTRY_POINT_COUNT = 3;
-constexpr const int  ENTRY_POINT = 1;
-constexpr const int  ROBOT_COUNT = 2;
+constexpr const int PLAN_TYPE = 1;
+constexpr const int PLAN_ID = 1;
+constexpr const int ENTRY_POINT_COUNT = 3;
+constexpr const int ENTRY_POINT = 1;
+constexpr const int ROBOT_COUNT = 2;
 
 // Plan Tree Info
 constexpr const int STATE_ID_COUNT = 3;
@@ -26,3 +26,7 @@ constexpr const int SUCCEEDED_EPS_COUNT = 3;
 
 // Role Switch
 constexpr const int ROLE_ID = 1;
+
+// Solver Var
+constexpr const int SOLVER_VAR_ID = 1;
+constexpr const int SOLVER_VAR_VALUE_SIZE = 4;
---
+++ test/src/conversion/solver_var/solver_var_from_capnp.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include <capnp/message.h>
+#include <SolverResult.capnp.h>
+#include <capnp/serialize.h>
+#include <model/SolverVar.h>
+#include <test_values_common.h>
+
+kj::Array<capnp::word> solver_var_message() {
+    capnp::MallocMessageBuilder builder;
+    auto solverVar = builder.initRoot<alica_msgs::SolverVar>();
+    solverVar.setId(SOLVER_VAR_ID);
+    auto value = solverVar.initValue(SOLVER_VAR_VALUE_SIZE);
+    for(int i = 0; i < SOLVER_VAR_VALUE_SIZE; i++) {
+        value.set(i, i);
+    }
+
+    return capnp::messageToFlatArray(builder);
+}
+
+TEST(SolverVarFromCapnp, it_can_be_parsed) {
+    auto message = solver_var_message();
+    capnp::FlatArrayMessageReader reader(message);
+
+    EXPECT_NO_THROW(SolverVar::from(reader));
+}
+
+TEST(SolverVarFromCapnp, it_can_no_be_parsed_if_value_is_missing) {
+    capnp::MallocMessageBuilder builder;
+    builder.initRoot<alica_msgs::SolverVar>();
+    auto message = capnp::messageToFlatArray(builder);
+    capnp::FlatArrayMessageReader reader(message);
+
+    EXPECT_THROW(SolverVar::from(reader), std::runtime_error);
+}
+
+TEST(SolverVarFromCapnp, it_can_not_parse_other_messages) {
+    capnp::MallocMessageBuilder builder;
+    builder.initRoot<alica_msgs::SolverResult>();
+    auto message = capnp::messageToFlatArray(builder);
+    capnp::FlatArrayMessageReader reader(message);
+
+    EXPECT_THROW(SolverVar::from(reader), std::runtime_error);
+}
+
+TEST(SolverVarFromCapnp, it_contains_the_id) {
+    auto message = solver_var_message();
+    capnp::FlatArrayMessageReader reader(message);
+
+    auto solverVar = SolverVar::from(reader);
+
+    EXPECT_EQ(solverVar.getId(), SOLVER_VAR_ID);
+}
+
+TEST(SolverVarFromCapnp, it_contains_the_value) {
+    auto message = solver_var_message();
+    capnp::FlatArrayMessageReader reader(message);
+
+    auto solverVar = SolverVar::from(reader);
+    auto solverVarValue = solverVar.getValue();
+
+    EXPECT_EQ(solverVarValue.size(), SOLVER_VAR_VALUE_SIZE);
+    for(int i = 0; i < SOLVER_VAR_VALUE_SIZE; i++) {
+        EXPECT_EQ(solverVarValue[i], i);
+    }
+}
