82fc2b70394e5c53486af3f494ef2823129d3896
Sven Starcke <sst97@gmx.net>
2020-08-07 12:04:49 +0200

    adds convenience header for conversion part

---
+++ include/conversion.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <model/AlicaEngineInfo.h>
+#include <model/AllocationAuthorityInfo.h>
+#include <model/PlanTreeInfo.h>
+#include <model/RoleSwitch.h>
+#include <model/SolverResult.h>
+#include <model/SyncReady.h>
+#include <model/SyncTalk.h>
