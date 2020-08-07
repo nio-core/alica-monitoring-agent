600ab64b247473182e0f7ab01f3bedb9ae738019
Sven Starcke <sst97@gmx.net>
2020-08-07 11:24:31 +0200

    adds conversion model for Sync Data

---
+++ test/src/conversion/sync_data/sync_data_to_json.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+#include <rapidjson/document.h>
+#include <model/capnzero/Id.h>
+#include <test_values_common.h>
+#include <model/SyncData.h>
+
+SyncData sync_data() {
+    capnzero::Id robotId(ID_TYPE, std::vector<uint8_t>(ID_VALUE.begin(), ID_VALUE.end()));
+    return {
+        robotId,
+        TRANSITION_ID,
+        TRANSITION_HOLDS,
+        ACK
+    };
+}
+
+TEST(SyncDataToJson, it_is_an_object)
+{
+    const std::string json = sync_data().toJson();
+
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    EXPECT_TRUE(doc.IsObject());
+}
+
+TEST(SyncDataToJson, it_contains_the_robot_id)
+{
+    const std::string json = sync_data().toJson();
+
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    EXPECT_TRUE(doc["robotId"].IsObject());
+    EXPECT_EQ(doc["robotId"]["type"].GetInt(), ID_TYPE);
+    EXPECT_EQ(doc["robotId"]["value"].GetString(), ID_VALUE);
+}
+
+TEST(SyncDataToJson, it_contains_the_transition_id)
+{
+    const std::string json = sync_data().toJson();
+
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    EXPECT_EQ(doc["transitionId"].GetInt64(), TRANSITION_ID);
+}
+
+TEST(SyncDataToJson, it_contains_the_transition_hold_status)
+{
+    const std::string json = sync_data().toJson();
+
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    EXPECT_EQ(doc["transitionHolds"].GetBool(), TRANSITION_HOLDS);
+}
+
+TEST(SyncDataToJson, it_contains_the_acknowledgement_status)
+{
+    const std::string json = sync_data().toJson();
+
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    EXPECT_EQ(doc["ack"].GetBool(), ACK);
+}
